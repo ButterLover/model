58e9bb72735a64a3fb0e8a236095fee942547d9d
NI <NI@BACH.eit.lth.se>
2016-06-02 16:01:10 +0200

    add title for rotating plot

--- tx1/plotRotate.m
+++ tx1/plotRotate.m
@@ -7,7 +7,7 @@ rot_angle=linspace(-180, 180, 61);
 rx_ind=[ 10 19 44 67 82 90 114 122];
 %
 load(strcat(pwd, '\rotate\iso\capacity'))
-for rxi=1:8
+for rxi=1:length(rx_ind)
     figure
     plot(rot_angle, cpbf(rxi, :),'-x','MarkerSize',3)
     hold on
@@ -17,13 +17,32 @@ for rxi=1:8
     hold on
     plot(rot_angle, cpsm2(rxi, :),'--')
     grid on
-    legend('BF','SM','SM4','SM2')
-    xlabel('Rotating angle \phi[\circ]');ylabel('Capacity [bps/Hz]');
-    %     saveas(gcf, strcat('rotISOrx', num2str(rxi)),'png')
+    legend('Location', 'best','BF','SM','SM4','SM2')
+    xlabel('\phi[\circ]');ylabel('Capacity [bps/Hz]');
+    title( strcat('Isotropic Rx #', num2str(rx_ind(rxi))))
+    saveas(gcf, strcat('rotISOrx', num2str(rx_ind(rxi))),'png')
+end
+for rxi=1:length(rx_ind)
+    figure
+    cdfplot( cpsm(rxi, :));
+    hold on
+    h=cdfplot( cpbf(rxi, :));
+    h.LineStyle='--';
+    hold on
+    h=cdfplot( cpsm4(rxi, :));
+    h.LineStyle='-.';
+    hold on
+    h=cdfplot( cpsm2(rxi, :));
+    h.LineStyle=':';
+    grid on
+    legend('Location', 'best', 'SM', 'BF', 'Hybrid SM4', 'Hybrid SM2')
+    xlabel('Capacity [bps/Hz]');ylabel('Probability');
+    title( strcat('Isotropic Rx #', num2str(rx_ind(rxi))))
+    saveas(gcf, strcat('rotISOcdfRx',  num2str(rx_ind(rxi))),'png')
 end
 
 load(strcat(pwd, '\rotate\patch\capacity'))
-for rxi=1:8
+for rxi=1:length(rx_ind)
     figure
     plot(rot_angle, cpbf(rxi, :),'-x','MarkerSize',3)
     hold on
@@ -33,8 +52,27 @@ for rxi=1:8
     hold on
     plot(rot_angle, cpsm2(rxi, :),'--')
     grid on
-    legend('BF','SM','SM4','SM2')
-    xlabel('Rotating angle \phi[\circ]');ylabel('Capacity [bps/Hz]');
-    %     saveas(gcf, strcat('rotPATrx', num2str(rxi)),'png')
+    legend('Location', 'best','BF','SM','SM4','SM2')
+    xlabel('\phi[\circ]');ylabel('Capacity [bps/Hz]');
+    title( strcat('Patch Rx #', num2str(rx_ind(rxi))))
+    saveas(gcf, strcat('rotPATrx',  num2str(rx_ind(rxi))),'png')
+end
+for rxi=1:length(rx_ind)
+    figure
+    cdfplot( cpsm(rxi, :));
+    hold on
+    h=cdfplot( cpbf(rxi, :));
+    h.LineStyle='--';
+    hold on
+    h=cdfplot( cpsm4(rxi, :));
+    h.LineStyle='-.';
+    hold on
+    h=cdfplot( cpsm2(rxi, :));
+    h.LineStyle=':';
+    grid on
+    legend('Location', 'best', 'SM', 'BF', 'Hybrid SM4', 'Hybrid SM2')
+    xlabel('Capacity [bps/Hz]');ylabel('Probability');
+    title( strcat('Patch Rx #', num2str(rx_ind(rxi))))
+    saveas(gcf, strcat('rotPATcdfRx',  num2str(rx_ind(rxi))),'png')
 end
 
